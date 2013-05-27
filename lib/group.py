2a34df1a647fd82e32e58ee924e157ec638f4bdf
maloi <maloi@localhost>
2013-05-27 12:07:44 +0200

    refactoring

--- lib/group.py
+++ lib/group.py
@@ -1,4 +1,4 @@
-from ldapiapp import LdapIapp
+from ldapiapp import LdapIapp, entry_from_ldap
 from django.conf import settings
 import re
 
@@ -19,9 +19,9 @@ class Group():
         for group in search_result:
             # schiebt windowssystem gruppen ins abseits, regex sollte verbessert werden, ggf. anderes ausschlusskriterium
             if re.match(r'^[A-Z]', group['cn'][0]):
-                system_groups.append(group_from_ldap(group, attributes))
+                system_groups.append(entry_from_ldap(Group, group, attributes))
             else:
-                groups.append(group_from_ldap(group, attributes))
+                groups.append(entry_from_ldap(Group, group, attributes))
         return groups
 
     @staticmethod
@@ -33,21 +33,5 @@ class Group():
         ldap = LdapIapp()
         search_result = ldap.getEntries(settings.LDAP_GROUP_DN, 'cn={0}'.format(cn), attributes)
         group = search_result[0]
-        return group_from_ldap(group, attributes)
+        return entry_from_ldap(Group, group, attributes)
 
-def group_from_ldap(search_result, attributes):
-    kwargs = {}
-    get_all = False
-    if len(attributes) == 0:
-        get_all = True
-    for key, value in search_result.items():
-        if not key in Group.MULTIVALUE_ATTRS:
-            value = value[0]
-        if not get_all and key in attributes:
-            kwargs[key] = value
-            attributes = [x for x in attributes if x != key]
-        else:
-            kwargs[key] = value
-        for key in attributes:
-            kwargs[key] = ''
-    return Group(**kwargs)
